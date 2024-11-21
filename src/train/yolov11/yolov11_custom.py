d604f478e9b05bc9662f45712f07db6727a7c0b0
hongdangn <ddangdangnh@gmail.com>
2024-11-21 21:24:29 +0700

    fix custom

--- src/train/yolov11/yolov11_custom.py
+++ src/train/yolov11/yolov11_custom.py
@@ -6,7 +6,7 @@ def train_yolov11_custom(weight_path, num_epochs, wandb_key, data_yaml):
     wandb.login(key=wandb_key)
 
     model = YOLO(weight_path)
-    results = model.train(data=data_yaml, epochs=num_epochs, imgsz=1280, momentum=0.737, weight_decay=0.0001, warmup_bias_lr=0.01, box=10, cls=3, mosaic=0.5, batch=4)
+    results = model.train(data=data_yaml, epochs=num_epochs, imgsz=1280, momentum=0.737, weight_decay=0.0001, warmup_bias_lr=0.01, box=10, cls=2, mosaic=0.5, batch=4)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Train yolov11 custom")
